bd3324b4bf75027de0f1300bc39f8251b039702c
Vaibhav522 <vp466801@gmail.com>
2022-01-31 15:03:42 +0530

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import "./style.css";
+import Topbar from "/Component/Home/Top/topbar.js"
 
 export default function App() {
   return (
     <div>
+      <Topbar />
       <h1>Hello Sanjana Mer i jaaam </h1>
     </div>
   );
