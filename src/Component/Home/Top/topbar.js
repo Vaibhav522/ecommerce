3b311747b0c5c7eac1a24ebfa718a0b6c665186e
Vaibhav522 <vp466801@gmail.com>
2022-01-31 15:58:10 +0530

    1 file modified

--- src/Component/Home/Top/topbar.js
+++ src/Component/Home/Top/topbar.js
@@ -4,7 +4,7 @@ import "./topbar.css";
 export default function Topbar() {
   return (
     <section id="top">
-      <img className="top_logo" src="/logo.svg"/>
+      <img className="top_logo" src="https://24e26d91-78c6-4d45-ac6a-a41270d60b50.id.repl.co/logo.svg"/>
     </section>
   );
 }
